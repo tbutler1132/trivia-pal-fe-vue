395c1afe450d47f3ad4fcd11a0b9695459de0be9
Timothy Butler <tbutler1132@gmail.com>
2021-12-09 12:21:19 -0500

    Init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Lobby />
+    <b-button>Button</b-button>
+  </div>
+</template>
+
+<script>
+import Lobby from './components/Lobby.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Lobby
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
